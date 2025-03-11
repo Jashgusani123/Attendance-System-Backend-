df61bfac2353cd8c3bef3c2a97b5489ce9d69458
Jash Gusani <jashgusani20@gmail.com>
2025-03-11 23:32:19 +0530

    Starting To Next Phase

---
+++ Utils/GetDays.ts
@@ -0,0 +1,15 @@
+export const GetLastDays = (num : number)=>{
+    const today = new Date();
+    let endDay ;
+    if(today.getDate() < num){
+        endDay = num - today.getDate();
+    }else{
+        endDay = today.getDate(); - num;
+    }
+
+    const endDate = new Date(today.getFullYear() , today.getMonth() , endDay - 1);
+    
+    return endDate;
+
+}
+
