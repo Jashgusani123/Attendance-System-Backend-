c37460b4fed24062f6e62a93d61c2a450c88325b
Jash Gusani <jashgusani20@gmail.com>
2025-03-13 21:48:41 +0530

    Changes For Checking

--- app.ts
+++ app.ts
@@ -27,7 +27,7 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: ["https://attendance-system-txfn.onrender.com", "http://localhost:5173" , process.env.FRONTEND!],
+  origin: ["https://attendance-system-txfn.onrender.com", "http://192.168.0.192:5173/", "http://localhost:5173" , process.env.FRONTEND!],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
@@ -45,8 +45,9 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "http://192.168.0.192:5173",
-      "https://attendance-system-txfn.onrender.com"
+      "http://192.168.0.192:5173/",
+      "https://attendance-system-txfn.onrender.com",
+      process.env.FRONTEND!
     ],
     credentials: true,
   },
